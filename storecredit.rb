4dfa3e944c3f2c7e0597c9d5ae8f4ce9e009d13e
Soo Philip Kim <philipjkim@gmail.com>
2012-03-21 17:04:40 +0900

    almost there, but consider when two valid prices are equal

--- storecredit.rb
+++ storecredit.rb
@@ -15,10 +15,21 @@ if __FILE__ == $0
 	num_cases = Integer(lines[0])
 	1.upto(num_cases) do |case_number|
 		credit_line_number = (case_number * 3) - 2
+		credit = Integer(lines[credit_line_number])
 		prices_line_number = (case_number * 3)
-		print "Case No. #{case_number}: ",
-			"credit=#{lines[credit_line_number]}, ",
-			"prices=#{lines[prices_line_number]}\n"
+		prices = lines[prices_line_number].split.map{|price| Integer(price)}
+
+		valid_pairs = nil
+		prices.each do |price|
+			expected_price = credit - price
+			if prices.include?(expected_price)
+				valid_pairs = [prices.index(price)+1,
+								prices.index(expected_price)+1]
+				break
+			end
+		end
+
+		puts "Case ##{case_number}: #{valid_pairs.join(' ')}"
 	end
 
 end
