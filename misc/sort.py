de1b25714ad1575d251dd48194a9aab36fd0faf8
Soo Philip Kim <philipjkim@gmail.com>
2012-03-31 22:41:56 +0900

    add logging features

--- misc/sort.py
+++ misc/sort.py
@@ -1,41 +1,60 @@
 #!/usr/bin/env python
 
 import sys
+from logging import *
 from random import *
+
+basicConfig(level=INFO)
 
 SIZE = 100
 
+def swap(arr, idx1, idx2):
+    temp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = temp
+
 def sort(l):
-    print "## Unknown sort"
-    numbers = l[:]
-    print "Unsorted: ", numbers
+    debug("## Unknown sort")
+    arr = l[:]
+    debug("Unsorted: %s" % arr)
 
-    for i in range(len(numbers)):
-        for j in range(i, len(numbers)):
-            if numbers[i] > numbers[j]:
-                t = numbers[i]
-                numbers[i] = numbers[j]
-                numbers[j] = t
-                #print "Swapped: ", numbers
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                swap(arr, i, j)
+                debug("Swapped: %s" % arr)
 
-    print "Sorted: ", numbers
+    debug("Sorted: %s" % arr)
 
 def bubblesort(l):
-    print "## Bubble sort"
-    numbers = l[:]
-    print "Unsorted: ", numbers
+    debug("## Bubble sort")
+    arr = l[:]
+    debug("Unsorted: %s" % arr)
 
     swapped = True
     while (swapped):
         swapped = False
-        for i in range(len(numbers)-1):
-            if numbers[i] > numbers[i+1]:
-                t = numbers[i]
-                numbers[i] = numbers[i+1]
-                numbers[i+1] = t
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                swap(arr, i, i+1)
                 swapped = True
-                #print "Swapped: ", numbers
-    print "Sorted: ", numbers
+                debug("Swapped: %s" % arr)
+    debug("Sorted: %s" % arr)
+
+def selectionsort(l):
+    debug("## Selection sort")
+    arr = l[:]
+    debug("Unsorted: %s" % arr)
+
+    for i in range(len(arr)):
+        minIdx = i
+        for j in range(i+1, len(arr)):
+           if arr[minIdx] > arr[j]:
+               minIdx = j
+        if i != minIdx:
+            swap(arr, i, minIdx)
+            debug("Swapped: %s" % arr)
+    debug("Sorted: %s" % arr)
 
 if __name__ == "__main__":
     l = range(SIZE)
@@ -43,5 +62,7 @@ if __name__ == "__main__":
     from timeit import Timer
     t1 = Timer("sort(l)", "from __main__ import sort, l")
     t2 = Timer("bubblesort(l)", "from __main__ import bubblesort, l")
+    t3 = Timer("selectionsort(l)", "from __main__ import selectionsort, l")
     print t1.timeit(1)
     print t2.timeit(1)
+    print t3.timeit(1)
